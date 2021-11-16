e99a3dc985772a70c19cf92f8e8a288cdda933c0
Jordan Ponto <jponto93@gmail.com>
2021-11-16 14:26:26 -0600

    can select picks and dispatch the list of picks. need to set up saga to send list to server.

--- src/components/MakePickTableItem/MakePickTableItem.jsx
+++ src/components/MakePickTableItem/MakePickTableItem.jsx
@@ -1,7 +1,6 @@
 import { TableCell, TableRow } from '@mui/material';
 import { isValidElement, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import GameItem from '../GameItem/GameItem';
 
 
 function MakePickTableItem({ game }) {
@@ -12,25 +11,27 @@ function MakePickTableItem({ game }) {
 
 
     const teamSelector = (event) => {
-        setSelectedTeam( event.target.innerHTML )
-        console.log(selectedTeam);
-        dispatch({ type: 'ADD_PICK_SELECTION', payload: selectedTeam })
-
+        setSelectedTeam(event.target.innerHTML)
+        console.log(selectedTeam);// state is async, will not be updated in time
+        dispatch({
+            type: 'ADD_PICK_SELECTION',
+            payload: {
+                pick: event.target.innerHTML,
+                game_id: game.id
+            }
+        })
     } // end teamSelector
 
     return (
         <TableRow key={game.id}>
             <TableCell>{game.visitor_score}</TableCell>
-            <TableCell onClick={(event) => teamSelector(event)}>{game.visitor_id}</TableCell>
+            <TableCell onClick={(event) => teamSelector(event, game.id)}>{game.visitor_id}</TableCell>
             <TableCell>@</TableCell>
-            <TableCell onClick={(event) => teamSelector(event)}>{game.home_id}</TableCell>
-            {/* <GameItem game={game}/> */}
+            <TableCell onClick={(event) => teamSelector(event, game.id)}>{game.home_id}</TableCell>
             <TableCell>{game.home_score}</TableCell>
             <TableCell>{game.game_date}</TableCell>
             <TableCell>{game.game_time}</TableCell>
-            <TableCell><input 
-            required
-            maxLength={3}></input></TableCell>
+            <TableCell>{selectedTeam}</TableCell>
         </TableRow>
     )
 } // end MakePickTableItem
--- src/components/MakeYourPicks/MakeYourPicks.jsx
+++ src/components/MakeYourPicks/MakeYourPicks.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import {
@@ -9,32 +8,21 @@ import {
     TableHead,
     TableRow,
     Select,
-    MenuItem
+    MenuItem,
 } from '@mui/material'
 import MakePickTableItem from "../MakePickTableItem/MakePickTableItem";
 
 
 function MakeYourPicks() {
 
-    const [pickList, setPickList] = useState({
-        schedule_id: 0,
-        pick: ''
-    })
-
-    const handlePickSelect = (event, prop1, prop2, prop3) => {
-        console.log('event happened');
-        setPickList({
-            ...pickList,
-            [prop1]: prop2,
-            [prop3]: event.target.value,
-        })
-    }
+    const pickList = useSelector(store => store.selection)
 
     const dispatch = useDispatch();
     const games = useSelector(store => store.games)
 
-    const handlePickSubmit = () => {
+    const handlePickSubmit = (action) => {
         console.log('clicked submit');
+        dispatch({ type: 'SUBMIT_PICK_LIST', payload: pickList })
     } // end handleSubmit
 
     console.log('this is pickList', pickList);
@@ -85,43 +73,15 @@ function MakeYourPicks() {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {/* {games.map((game) => {
+                            {games.map((game) => {
                             return (
                                 <MakePickTableItem game={game}
                                 key={game.id}/>
                             )
-                        })} */}
-                            {games.map((game) => {
-                                return (
-                                    <TableRow key={game.id}>
-                                        <TableCell>{game.visitor_score}</TableCell>
-                                        <TableCell>{game.visitor_id}</TableCell>
-                                        <TableCell>@</TableCell>
-                                        <TableCell>{game.home_id}</TableCell>
-                                        {/* <GameItem game={game}/> */}
-                                        <TableCell>{game.home_score}</TableCell>
-                                        <TableCell>{game.game_date}</TableCell>
-                                        <TableCell>{game.game_time}</TableCell>
-                                        <TableCell>
-                                            <form id="myForm" onSubmit={handlePickSubmit}>
-                                            <input
-                                                onChange={(event) => handlePickSelect(event, 'schedule_id', game.id, 'pick')}
-                                                id={game.id}
-                                                name="pick"
-                                                maxLength="3"
-                                                required
-                                                type="text"
-                                                form="myForm"></input>
-                                            <button type="submit" form="myForm">SUBMIT</button>
-                                        </form>
-                                    </TableCell>
-
-                                </TableRow>
-                        )
                         })}
                     </TableBody>
                 </Table>
-                
+                <button onClick={handlePickSubmit}>SUBMIT PICKS</button>
                 
             </TableContainer> : <h2>SELECT YOUR WEEK</h2>
 }
