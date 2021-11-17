a88657d8a57361eb7c2ec68f70804b27c7943a01
Jordan Ponto <jponto93@gmail.com>
2021-11-17 10:31:52 -0600

    added email to register form to help populate data that admin will use

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,17 +1,22 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 
 function Admin () {
 
     const dispatch = useDispatch();
+    const membersList = useSelector(store => store.members)
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MEMBERS' })
     }, [])
-    
+
     return (
+        <>
         <h1>WELCOME, ADMIN</h1>
+            
+        </>
+
     )
 } // end Admin
 
