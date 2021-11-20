dea7adf7f9296519a0170a9d68a908521b4ba200
Jordan Ponto <jponto93@gmail.com>
2021-11-20 10:52:30 -0600

    changed pool.query() in router

--- server/routes/member.details.router.js
+++ server/routes/member.details.router.js
@@ -3,28 +3,29 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const {
-  rejectUnauthenticated,
+    rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
 router.get('/', (req, res) => {
     // GET route code here
     console.log('you made in through to member.detail router');
-     const {id, week} = req.query
-     console.log('req.query', req.query);
-    
+    const { id, week } = req.query
+    console.log('req.query', req.query);
+
     // get all users that are not the admin
-      const sqlText = `
+    const sqlText = `
         SELECT * FROM "picks"
         WHERE "user_id" = $1 AND "week" = $2;
         `;
-      pool.query(sqlText, [Number(id), Number(week)])
-      .then(response => {
-          console.log('this is response.rows', response.rows);
-          res.send(response.rows)
-      })
-      .catch(err => {
-          console.log('error in member get', err);
-      })
-    });
+    const values = [id, week]
+    pool.query(sqlText, values)
+        .then(response => {
+            console.log('this is response.rows', response.rows);
+            res.send(response.rows)
+        })
+        .catch(err => {
+            console.log('error in member get', err);
+        })
+});
 
 module.exports = router;
